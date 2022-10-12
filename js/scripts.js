70a62c93767c22e51003b1663075c366921b9094
tarikochuery <ttarikechuery@gmail.com>
2022-10-12 10:24:39 -0300

    V1.0 - Implemented funcional modal

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@ const dishes = document.getElementById('dishes')
 const beverages = document.getElementById('beverages')
 const desserts = document.getElementById('desserts')
 const orderButton = document.getElementById('order-button')
+const confirmButton = document.getElementById('confirm-btn')
+const cancelButton = document.getElementById('cancel-btn')
 
 const dishesArray = Array.from(dishes.children)
 const beveragesArray = Array.from(beverages.children)
@@ -19,6 +21,29 @@ const order = {
     desserts: {
         name: '',
         value: 0
+    }
+}
+
+const buildModalInformation = () => {
+    const orderInfoElement = document.getElementById('order-info')
+    const orderInfoArray = Array.from(orderInfoElement.children)
+    const orderEntries = Object.keys(order)
+
+    orderInfoArray.forEach((item, idx) => {
+        if (idx > 2) return
+        item.firstElementChild.innerHTML = order[orderEntries[idx]].name
+        item.lastElementChild.innerHTML = `R$ ${order[orderEntries[idx]].value.toFixed(2).replace('.', ',')}`
+    })
+
+    orderInfoElement.lastElementChild.lastElementChild.innerHTML = `R$ ${(order.dishes.value+order.beverages.value+order.desserts.value).toFixed(2).replace('.', ',')}`
+}
+
+const toggleModal = () => {
+    const modal = document.getElementById('modal')
+    modal.classList.toggle('noshow')
+
+    if (!modal.classList.contains('noshow')) {
+        buildModalInformation()
     }
 }
 
@@ -47,12 +72,14 @@ const makeOrder = () => {
 
     const orderURIEncoded = encodeURIComponent(ORDER_TEXT)
 
-    window.location.href = `https://wa.me/5511941471883?text=${orderURIEncoded}`
+    window.open(`https://wa.me/5511941471883?text=${orderURIEncoded}`, '_blank')
 }
 
 const addProductInformationToOrder = (element) => {
     order[element.parentElement.id].name = element.querySelector('.option-name').innerHTML
     order[element.parentElement.id].value = Number(element.querySelector('.option-price').innerHTML.split('R$ ')[1].replace(',', '.'))
+
+
 }
 
 const createCheckMarker = () => {
@@ -93,4 +120,6 @@ const selectOption = (element) => {
 dishesArray.forEach(card => card.addEventListener('click', () => selectOption(card)))
 beveragesArray.forEach(card => card.addEventListener('click', () => selectOption(card)))
 dessertsArray.forEach(card => card.addEventListener('click', () => selectOption(card)))
-orderButton.addEventListener('click', makeOrder)
+orderButton.addEventListener('click', toggleModal)
+cancelButton.addEventListener('click', toggleModal)
+confirmButton.addEventListener('click', makeOrder)
